2dfe942cf7044ed96afcdc0f06d6cfa52588e8cf
MasterAI-Official <viratvaus45@gmail.com>
2026-04-28 14:22:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ try:
     # Secrets se key uthana
     api_key = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
-    # Sab se stable model use karna
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    # پرانی لائن کو اس سے بدل دیں
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
+
 
 except Exception as e:
     st.error("Secrets configuration error!")
